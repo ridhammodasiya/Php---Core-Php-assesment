c432d2e1f90623241f6c833fa9c30942a4d10b68
ridhammodasiya <148623365+ridhammodasiya@users.noreply.github.com>
2024-09-25 17:32:49 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+$name ="php programming";
+print($name);
+echo gettype($name);
+
+?>
+
+#php programming:
+#data type:
+#int,float,double,string,boolen
+#arry:collection of intl_error_name
+#stream_set_blocking
+
+// $data= arry("apple,orange,pinaple")
+
+// echo gettype($data);
+
+// $data = 256;
+// echo gettype($data);
+
+//null
+//bool,
+//int,
+//float(floating point number)
+//string.
+//arry,
+//object,
+//callable
+//resource.
+
+//$age
+//print(gettype($age))
+
+$c = $a / $b;
+
+//$age=25
+
+//echo $age;
+
+//$age=25;
+//$age =(float)($age);
+//echo $age;
+
+//$salary =3.65478;
+//printf(gettype($salary));
+
+//echo $salary.php_eol;
+
+//$salary =(int)($salary);
+
+//echo $salary
+
+//$data = arry("apple",orang,pineaple);
+
+// var_dump($data)
+
+string:
+arry : collection
---
+++ pra2.php
@@ -0,0 +1,14 @@
+<?php
+$cm=readline("enter your cm:");
+$ft=$cm*0.0328;
+print("total ft is:$ft");
+
+$kilometer=readline("enter your kilometers:");
+$mile=$kilometer*0.621;
+print("total mile is:$mile");
+
+$doller=readline("enter doller:");
+$inr=$doller*80;
+print("total inr is:$inr");
+
+?>
